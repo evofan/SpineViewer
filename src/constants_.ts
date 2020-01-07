4870e6d93eefdec38cf69f94d4289f6d03f3a5a8
evofan <evofan@outlook.jp>
2020-01-07 22:11:09 +0900

    Change get pointer(x, y), fix

---
+++ src/constants_.ts
@@ -0,0 +1,20 @@
+// stage settings
+export const STAGES = {
+  WIDTH: 720,
+  HEIGHT: 480,
+  BG_COLOR: 0x000000
+};
+
+// path for use assets
+export const ASSETS = {
+  ASSET_BG: "assets/images/pic_bg.jpg", // your bg image
+  // ASSET_BG: "", // if you don't want to use bg image
+  ASSET_SPINE1: "assets/spine/spineboy/export/spineboy.json" // your spine animation
+  // ASSET_SPINE1: "assets/spine/alien/export/alien.json" // your spine animation
+  // ASSET_SPINE1: "assets/spine/dragon/export/dragon.json" // your spine animation
+  // ASSET_SPINE1: "assets/spine/powerup/export/powerup.json" // your spine animation
+};
+
+export const GAMES = {
+  FPS: 60 // framerate ex. 30
+};
